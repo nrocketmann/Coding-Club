61200e6ea99f0445aa68eec508b9f1b4f9dd8059
190n <ben@190n.org>
2016-11-05 10:53:37 -0700

    Start working on accessing API

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,1 +1,19 @@
+// which "instance" of the app we are using
+// <this>.myschoolapp.com
+var basename = 'lwhs';
 
+// retrieves the token cookie and calls a callback function with the token
+function getToken(cb) {
+  chrome.cookies.get({
+    url: basename + '.myschoolapp.com/',
+    name: 't'
+  }).then(cb);
+}
+
+window.addEventListener('load', function() {
+  document.getElementById('view-token').onclick = function() {
+    getToken(function(t) {
+      document.getElementById('token').textContent = t;
+    });
+  };
+});
