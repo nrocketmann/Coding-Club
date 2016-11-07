6fb59a16f6bcbd6f6b47dc8550d4eeb71d50a956
190n <ben@190n.org>
2016-11-07 11:44:28 -0800

    Got API working

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -22,20 +22,23 @@ const API_FILTER_ASSIGNED = 0,
 // https://lwhs.myschoolapp.com/api/DataDirect/AssignmentCenterAssignments/?format=json&filter=2&dateStart=11/6/2016&dateEnd=11/6/2016&persona=2&statusList=&sectionList=
 // gets assignment information from API
 function getAssignments(filter, startDate, endDate, token, cb) {
-  var url = 'https://' + basename + '.myschoolapp.com/api/DataDirect/AssignmentCenterAssignments/?format=json&persona=2&filter='\
-    + filter + '&dateStart=' + formatDate(startDate) + '&dateEnd=' + formatDate(endDate) + '&t=' + token,
+  var url = ('https://' + basename + '.myschoolapp.com/api/DataDirect/AssignmentCenterAssignments/?format=json&persona=2&filter='
+    + filter + '&dateStart=' + formatDate(startDate) + '&dateEnd=' + formatDate(endDate) + '&t=' + token),
     xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
   xhr.onload = function() {
-    // todo
+    cb(JSON.parse(xhr.responseText));
   };
   xhr.send();
 }
 
-document.getElementById('view-token').addEventListener('click', function() {
-  document.getElementById('token').innerHTML = '<i>loading...</i>';
-  getToken(function(t) {
-    if (t === null) document.getElementById('token').innerHTML = 'null';
-    else document.getElementById('token').innerHTML = t;
-  });
+window.addEventListener('load', function() {
+  document.getElementById('view-token').addEventListener('click', function() {
+    document.getElementById('token').innerHTML = '<i>loading...</i>';
+    getToken(function(t) {
+      getAssignments(API_FILTER_DUE, new Date(), new Date(), t, function(data) {
+        document.getElementById('token').innerHTML = '<pre>' + JSON.stringify(data) + '</pre>';
+      });
+    });
+  }, false);
 }, false);
