27fdb02a7d8301d17ebc06ff7d948ff923458e7c
nrocketmann <nameerhirschkind@gmail.com>
2017-04-27 08:37:55 -0700

    correct imports

--- chrome-extension/app-ui/src/App.js
+++ chrome-extension/app-ui/src/App.js
@@ -10,7 +10,8 @@ import Ui4 from './Ui';
 import Ui5 from './Ui';
 import Ui6 from './Ui';
 import Ui7 from './Ui';
-import * as popup from '../../src/scripts/popup.js';
+import getAssignments as AsRaw from '../../src/scripts/popup.js';
+import parseAssignmentData as AsParsed from '../../src/scripts/popup.js';
 import Home from './Home';
 import './App.css'
 // import example from '../../example.json'
